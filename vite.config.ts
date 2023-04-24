cd3812848bcbd2f0c6b7b06b952709b3667ffe0a
wanter <miss.you.da.bao@protonmail.com>
2023-04-24 10:40:23 +0800

    配置后端接口打通 和flask打通

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,36 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import VueSetupExtend from 'vite-plugin-vue-setup-extend';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers';
+
 export default defineConfig({
-	base: './',
-	plugins: [
-		vue(),
-		VueSetupExtend(),
-		AutoImport({
-			resolvers: [ElementPlusResolver()]
-		}),
-		Components({
-			resolvers: [ElementPlusResolver()]
-		})
-	],
-	optimizeDeps: {
-		include: ['schart.js']
+    base: './',
+    plugins: [
+        vue(),
+        VueSetupExtend(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()]
+        })
+    ],
+	server: {
+		proxy: {
+			"/api": {
+				//本地地址
+				target: "http://127.0.0.1:5000",
+				//允许跨域
+				changeOrigin: true,
+				// 访问后端地址时候，将api后缀干掉
+				rewrite: (path => path.replace(/^\/api/, ""))
+			}
+		}
 	}
+	,
+    optimizeDeps: {
+        include: ['schart.js']
+    }
 });
